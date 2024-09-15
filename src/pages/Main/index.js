67449eade9cb55fd10e6ba3e53928eb3309bbc66
gogoncalves <gustavo.goncalves@sptech.school>
2024-09-15 01:03:40 -0300

    feat: adding styles for main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
     return(
-      <Title>Main</Title>  
+      <Container>
+        
+        <h1>        
+            <FaGithub size={25} />
+            My Repositories
+        </h1>
+        
+        <Form>
+            <input type='text' placeholder='Add Repositories' />
+            <SubmitButton>
+            <FaPlus color='#fff' size={14}/>
+
+            </SubmitButton>
+        </Form>
+
+      </Container>  
     );
 }
